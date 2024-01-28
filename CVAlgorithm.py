e0a0fd0d3159d081e6d29a1864515668176e2f00
Danny Esaid <dannyesaid@gmail.com>
2024-01-27 17:06:43 -0800

    Update CVAlgorithm.py

--- CVAlgorithm.py
+++ CVAlgorithm.py
@@ -4,18 +4,12 @@ import sys
 from Queue import Queue
 from PIL import Image
 
-
-
 maze_image = cv2.imread('testfive.jpg')
-
 maze_image_black_and_white = cv2.cvtColor( maze_image, cv2.COLOR_BGR2GRAY )
-
 unneeded_object, maze_image_thresholded = cv2.threshold( maze_image_black_and_white, 127, 255, cv2.THRESH_BINARY)
-
 maze_image_height, maze_image_width =  maze_image.shape[ : 2 ]
 
 start = (21, 495)
-
 end = (508,517)
 
 def iswhite(value):
@@ -35,7 +29,6 @@ def BFS(start, end, pixels):
     queue.put([start]) # Wrapping the start tuple in a list
 
     while not queue.empty():
-
         path = queue.get()
         pixel = path[-1]
 
@@ -57,37 +50,24 @@ def BFS(start, end, pixels):
 
     print "Queue has been exhausted. No answer was found."
 
-
-
 kernel_height = maze_image_height // 100
-
 kernel_width = maze_image_width // 100
-
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ( kernel_width,kernel_height ) )
 
 maze_image_with_closed_pixels = cv2.morphologyEx( maze_image_thresholded, cv2.MORPH_CLOSE, kernel )
-
 unneeded_object, maze_image_with_closed_pixels = cv2.threshold( maze_image_with_closed_pixels, 127, 255, cv2.THRESH_BINARY )
-
 final_maze_image = Image.fromarray(  maze_image_with_closed_pixels )  ##########
-
 final_maze_image.save( "temporary_results.jpg" )  ##############
 
-
-
 base_img = Image.open( 'temporary_results.jpg' ) # Image.open('temporary_results.jpg')
-
 base_pixels = base_img.load()
-
 path = BFS(start, end, base_pixels)
 
 path_img = Image.open('temporary_results.jpg')
 path_pixels = path_img.load()
 
 for position in path:
-
     x,y = position
-
     path_pixels[x,y] = 127
 
 path_img.save("final_resultsAA.jpg")
